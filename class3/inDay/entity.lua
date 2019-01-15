b633883ea688f6be4fb2f194286a642f004399e0
khwesten <k-heiner@hotmail.com>
2019-01-15 00:28:10 -0200

    Engine to collision and remotion from objects out of screen

---
+++ class3/inDay/entity.lua
@@ -0,0 +1,94 @@
+local Entity = new(Object) {
+  spec = nil,
+  cooldown = 0
+}
+
+function Entity.load(name)
+  return new(Entity) {
+    spec = new 'spec' (require('database.' .. name))
+  }
+end
+
+function Entity:init()
+  local memory_space = tostring(self)
+
+  self.id = memory_space:gsub(': ', '')
+  self.pos = self.pos or new(Vec) { 0, 0 }
+  self.dir = new(Vec) { 0, 0 }
+end
+
+function Entity:setDirection(dir)
+  self.dir = dir:normalized()
+end
+
+function Entity:update(entities, dt)
+  self.pos:translate(self.dir * self.spec.speed * dt)
+  self.cooldown = self.cooldown + dt
+
+  local cooldown_between_shots = self.spec.bullet_cooldown / self.spec.firerate
+
+  while self.cooldown > cooldown_between_shots do
+    self.cooldown = self.cooldown - cooldown_between_shots
+
+    local new_entity = Entity.load(self.spec.bullet)
+    new_entity.pos:set(self.pos.x, self.pos.y + self.spec.bullet_start)
+    new_entity:setDirection(new(Vec) (self.spec.bullet_direction))
+
+    add_entity(new_entity)
+  end
+
+  for _,entity in next, entities do
+    if self:check_collision(entity) and entity.spec.hide_on_die then
+      remove_entity(entity)
+    elseif entity:out_of_screen() then
+      remove_entity(entity)
+    end
+  end
+end
+
+function Entity:out_of_screen()
+  return self.pos.x > 400 or self.pos.x < 0 or self.pos.y > 600 or self.pos.y < 0
+end
+
+function Entity:check_collision(entity)
+  if entity == nil or self.id == entity.id then
+    return false
+  end
+
+  local x1 = self.pos.x
+  local y1 = self.pos.y
+  local r1 = self.spec.area_collision.params[4]
+
+  local x2 = entity.pos.x
+  local y2 = entity.pos.y
+  local r2 = entity.spec.area_collision.params[4]
+
+  local collided = (x1 - x2)^2 + (y2 - y1)^2 <= (r2 + r1)^2
+
+  if collided then
+    return true
+  end
+
+  return false
+end
+
+function Entity:draw()
+  local view = self.spec.view
+  local area_collision = self.spec.area_collision
+  local g = love.graphics
+
+  g.push()
+  g.translate(self.pos:get())
+  g.setColor(view.color)
+  g[view.type](unpack(view.params))
+  g.pop()
+
+  g.push()
+  g.translate(self.pos:get())
+  g.setColor(area_collision.color)
+  g[area_collision.type](unpack(area_collision.params))
+  g.pop()
+end
+
+return Entity
+
---
+++ class3/inDay/main.lua
@@ -0,0 +1,116 @@
+-- json = require 'dkjson'
+-- debuggee = require 'vscode-debuggee'
+-- startResult, breakerType = debuggee.start(json)
+
+-- print('debuggee start ->', startResult, breakerType)
+
+-- xpcall(
+--   function()
+--     -- Code to actually run
+--     local a = 1 + nil
+--   end,
+--   function(e)
+--     if debuggee.enterDebugLoop(1, e) then
+--       -- ok
+--     else
+--       -- If the debugger is not attached, enter here.
+--       print(e)
+--       print(debug.traceback())
+--     end
+--   end
+-- )
+
+require 'common'
+local Entity = require 'entity'
+
+local _player
+local _entity_timer
+
+function love.load()
+  W, H = love.graphics.getDimensions()
+
+  _ais = {}
+  _entities = {}
+  _objects_on_screen = 0
+  _entity_timer = 0
+  _player = Entity.load 'player'
+  _player.pos:set(W/2, 9*H/10)
+
+  love.graphics.print(_player.id, 100, 100)
+
+  add_entity(_player)
+end
+
+local CONTROLS = {
+  up = new(Vec) { 0, -1 },
+  down = new(Vec) { 0, 1 },
+  left = new(Vec) { -1, 0 },
+  right = new(Vec) { 1, 0 },
+}
+
+function love.update(dt)
+  -- Update entity timer
+  _entity_timer = _entity_timer + dt
+
+  if _entity_timer >= 2 then
+    -- Reseta contador
+    _entity_timer = 0
+
+    local ai = new 'ai' {}
+    local W = love.graphics.getDimensions()
+    local new_entity = Entity.load 'enemy'
+
+    new_entity.pos:set(love.math.random() * W, 10)
+    ai:setEntity(new_entity)
+
+    add_entity(new_entity, ai)
+  end
+
+  -- AI control
+  for _,ai in next, _ais do
+    ai:think()
+  end
+
+  -- Player control
+  local dir = new(Vec) {}
+
+  for key,control_dir in pairs(CONTROLS) do
+    if love.keyboard.isDown(key) then
+      dir:translate(control_dir)
+    end
+  end
+
+  _player:setDirection(dir)
+
+  -- Update entities
+  for _,entity in next, _entities do
+    entity:update(_entities, dt)
+  end
+end
+
+function love.draw()
+  love.graphics.print(_objects_on_screen, 10, 10)
+  for _,entity in next, _entities do
+    entity:draw()
+  end
+end
+
+function add_entity(entity, ai)
+  _entities[entity.id] = entity
+
+  if ai then
+    _ais[entity.id] = ai
+  end
+
+  _objects_on_screen = _objects_on_screen + 1
+end
+
+function remove_entity(entity)
+  if _ais[entity.id] then
+    _ais[entity.id] = nil
+  end
+
+  _entities[entity.id] = nil
+
+  _objects_on_screen = _objects_on_screen - 1
+end
