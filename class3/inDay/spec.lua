f38a131ea7d779980457092a89e6327dec84a9e2
khwesten <k-heiner@hotmail.com>
2019-01-15 00:27:24 -0200

    Some bullets config and area collision

---
+++ class3/inDay/spec.lua
@@ -0,0 +1,20 @@
+
+local Spec = new(Object) {
+  speed = 100,
+  firerate = 0,
+  bullet = nil,
+  bullet_direction = { 0, 0 },
+  bullet_cooldown = 1,
+  view = {
+    color = { 1, 1, 1 },
+    type = 'rectangle',
+    params = { 'fill', -5, -5, 10, 10 }
+  },
+  area_collision = {
+    color = { 0, 1, 0 },
+    type = 'circle',
+    params = { 'line',  10, 10, 10 }
+  },
+}
+
+return Spec
