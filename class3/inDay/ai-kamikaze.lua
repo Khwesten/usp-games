5ccc089ddfbae4341ff71b2fe7db42e4dcfdfcf8
khwesten <k-heiner@hotmail.com>
2019-01-17 18:37:51 -0200

    Adjust target kamikaze when play died

--- class3/inDay/ai-kamikaze.lua
+++ class3/inDay/ai-kamikaze.lua
@@ -9,9 +9,13 @@ function AI:setEntity(entity)
 end
 
 function AI:think()
-  self.target = new(Vec) {
-    _player.pos.x, _player.pos.y
-  }
+  local target = { love.math.random()*2*W - W/2, H+100 }
+
+  if _player.spec.health > 0 then
+    target = { _player.pos.x, _player.pos.y }
+  end
+
+  self.target = new(Vec) (target)
 
   self.direction = self.target - self.entity.pos
 
