297b4530da1975f2c4cc81fae9a3f27a8b6a164e
khwesten <k-heiner@hotmail.com>
2019-01-21 15:40:32 -0200

    Added score sum and abstract life to update

--- class3/inDay/entity.lua
+++ class3/inDay/entity.lua
@@ -50,12 +50,19 @@ function Entity:update(entities, dt)
 
   for _,entity in next, entities do
     if self:check_collision(entity) then
+      self.spec.health = self.spec.health - entity.spec.damage
+      entity.spec.health = entity.spec.health - self.spec.damage
+
       if entity.spec.hide_on_die and entity.spec.health <= 0 then
         remove_entity(entity)
       end
 
-      if self.type == 'bullet' and entity.type ~= 'bullet'
-        then remove_entity(self)
+      if self.type == 'bullet' and entity.type ~= 'bullet' then
+        remove_entity(self)
+      end
+
+      if entity.type == 'enemy' then
+        player_score = player_score + 1
       end
     end
 
@@ -99,9 +106,6 @@ function Entity:check_collision(entity)
   local collided = (x1 - x2)^2 + (y2 - y1)^2 <= (r2 + r1)^2
 
   if collided then
-    self.spec.health = self.spec.health - entity.spec.damage
-    entity.spec.health = entity.spec.health - self.spec.damage
-
     return true
   end
 
@@ -121,7 +125,7 @@ function Entity:draw()
 
   set_default_color()
 
-  if see_collision_area then
+  if game_debug_mode then
     g.push()
     g.translate(self.pos:get())
     g.setColor(area_collision.color)
